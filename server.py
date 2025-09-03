58795d2f0597ca675082bac6a7152e6027b6bb04
extremework205 <extremework205@gmail.com>
2025-09-03 02:37:29 +0100

    Update server.py

--- server.py
+++ server.py
@@ -118,6 +118,8 @@ def get_db():
 def create_tables():
     Base.metadata.create_all(bind=engine)
 
+logger = logging.getLogger("uvicorn.error")
+
 # =============================================================================
 # ENUMS
 # =============================================================================
